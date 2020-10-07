a8fef917c1d11ad7725543b4cbc186fc436f885f
Juerd Waalboer <juerd@tnx.nl>
2020-10-07 07:32:59 +0200

    Retain temperature offset

--- lib/Max/Device.pm
+++ lib/Max/Device.pm
@@ -116,9 +116,12 @@ sub config_temperatures {
     substr $self->{config}, 2, 1, pack("C", $max     * 2) if defined $max;
     substr $self->{config}, 3, 1, pack("C", $min     * 2) if defined $min;
 
-    return $self->_send_radio(
-        0x11,
-        unpack("H*", substr($self->{config}, 0, $self->{type} eq 'thermostat' ? 4 : 7))
-    );
+    my $config_data = $self->{type} eq 'thermostat'
+        ? substr($self->{config}, 0, 4) . substr($self->{config}, 186, 2)
+        # at offset 186: offset, window-open; duration not in {config}?
+        : substr($self->{config}, 0, 7);
+	# at offset 4: offset, window-open, window-duration
+
+    return $self->_send_radio(0x11, unpack("H*", $config_data));
 }
 1;
